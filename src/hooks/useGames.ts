87a7184c0cebaac79ce46a7cb44dcff26f1d566b
Orel <orelush555@gmail.com>
2024-01-24 16:57:04 +0200

    Building GameCards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
-import apiClient from "../../services/api-client";
+import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
+import { ImgProps } from "@chakra-ui/react";
 
-interface Game {
+export interface Game {
     id: number;
     name: string;
+    background_image: string;
   }
   
   interface FetchGamesResponse {
