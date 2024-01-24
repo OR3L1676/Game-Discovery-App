35ea86ef46278ab57ff022272394e3b08851dd89
Orel <orelush555@gmail.com>
2024-01-24 17:51:40 +0200

    Defining Platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,10 +3,18 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 import { ImgProps } from "@chakra-ui/react";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    icon: string;
+    parent_platforms: {platform: Platform}[]
   }
   
   interface FetchGamesResponse {
